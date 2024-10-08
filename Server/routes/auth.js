a7e44946846995a744762c98b8a41041ddd53ec1
Debapriyo Das <debapriyo00@gmail.com>
2024-10-09 00:05:23 +0530

    update backend

--- Server/routes/auth.js
+++ Server/routes/auth.js
@@ -1,75 +1,83 @@
-const express=require('express')
-const router=express.Router()
-const User=require('../models/User')
-const bcrypt=require('bcrypt')
-const jwt=require('jsonwebtoken')
+const express = require('express')
+const router = express.Router()
+const UserModel = require("../models/User")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
+// REGISTER
+router.post("/register", async(req, res)=>{ 
+    try {
+         const {username, email, password} = req.body
+         const userEmail = await UserModel.findOne({email})
+         if(email === userEmail){
+                return res.status(400).json("Email already exist!")
+         }
+         const hashedPassword = await bcrypt.hash(password, 10)
+         const newUser = new UserModel({username, email, password:hashedPassword})
+         const saveuser = await newUser.save()
 
-//REGISTER
-router.post("/register",async(req,res)=>{
-    try{
-        const {username,email,password}=req.body
-        const salt=await bcrypt.genSalt(10)
-        const hashedPassword=await bcrypt.hashSync(password,salt)
-        const newUser=new User({username,email,password:hashedPassword})
-        const savedUser=await newUser.save()
-        res.status(200).json(savedUser)
-
-    }
-    catch(err){
+         res.status(200).json({
+                message:"User has been registered successfully",
+                user:saveuser
+         })
+    } catch (err) {
         res.status(500).json(err)
     }
-
 })
 
-
-//LOGIN
-router.post("/login",async (req,res)=>{
-    try{
-        const user=await User.findOne({email:req.body.email})
-       
-        if(!user){
+// LOGIN
+router.post("/login", async(req, res)=>{
+    try {
+        // find user base on the email.
+        const user = await UserModel.findOne({email:req.body.email})
+        if(!user) {
             return res.status(404).json("User not found!")
         }
-        const match=await bcrypt.compare(req.body.password,user.password)
-        
-        if(!match){
-            return res.status(401).json("Wrong credentials!")
+        //compare password
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        if(!validPassword){
+            return res.status(400).json("Wrong password!")
         }
-        const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
-        const {password,...info}=user._doc
-        res.cookie("token",token).status(200).json(info)
-
-    }
-    catch(err){
+        //create token
+        const token = jwt.sign({_id:user._id, username:user.username, email:user.email}, process.env.SECRET, {expiresIn:"1d"})
+        const {password, ...info} = user._doc
+        res.cookie("token", token).status(200).json({
+            message:"Login successful",
+            info
+        })
+    } catch (err) {
         res.status(500).json(err)
     }
 })
 
-
-
-//LOGOUT
-router.get("/logout",async (req,res)=>{
-    try{
-        res.clearCookie("token",{sameSite:"none",secure:true}).status(200).send("User logged out successfully!")
-
-    }
-    catch(err){
+// LOGOUT
+router.get("/logout", (req, res)=>{
+    try {
+        res.clearCookie("token", {sameSite:"none", secure:true}).status(200).json({
+            message:"Logout successful!"
+        })
+    } catch (err) {
         res.status(500).json(err)
     }
 })
 
-//REFETCH USER
-router.get("/refetch", (req,res)=>{
-    const token=req.cookies.token
-    jwt.verify(token,process.env.SECRET,{},async (err,data)=>{
-        if(err){
-            return res.status(404).json(err)
+// REFETCH USER
+router.get('/refetch', (req, res) => {
+    const token = req.cookies.token;
+
+    // Check if token is present
+    if (!token) {
+        return res.status(400).json({ name: "JsonWebTokenError", message: "jwt must be provided" });
+    }
+
+    jwt.verify(token, process.env.SECRET, {}, async (err, data) => {
+        if (err) {
+            return res.status(404).json(err);
         }
-        res.status(200).json(data)
-    })
+        if (data) {
+            res.status(200).json(data);
+        }
+    });
 })
 
-
-
-module.exports=router
+module.exports = router
